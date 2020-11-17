dbe52b8d58c8677c61642a046bd6796d07329680
Ayush Jha <jhaayush1214@gmail.com>
2020-11-17 20:40:58 +0530

    Implemented Logout Functionality , Scraped the playlist function.

--- app/src/main/java/com/personal/todoist/PodcastFragment.java
+++ app/src/main/java/com/personal/todoist/PodcastFragment.java
@@ -1,16 +1,22 @@
 package com.personal.todoist;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,13 +26,17 @@ public class PodcastFragment extends Fragment implements View.OnClickListener {
     ImageButton playBtn;
     MediaPlayer mp;
     SeekBar seekBar;
+    ImageButton logout;
+    FirebaseAuth fAuth;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mView =  inflater.inflate(R.layout.fragment_podcast, container, false);
         playBtn = mView.findViewById(R.id.playBtn);
         seekBar =  mView.findViewById(R.id.seekBar);
+        logout = mView.findViewById(R.id.Logout);
         playBtn.setOnClickListener(this);
+        logout.setOnClickListener(this);
         mp = MediaPlayer.create(getActivity(),R.raw.furelise);
         return mView;
     }
@@ -34,15 +44,22 @@ public class PodcastFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        if (!mp.isPlaying()){
-            mp.start();
-            playBtn.setBackgroundResource(R.mipmap.ic_pause);
+        switch (view.getId()) {
+            case R.id.playBtn:
+                if (!mp.isPlaying()) {
+                    mp.start();
+                    playBtn.setBackgroundResource(R.mipmap.ic_pause);
+                } else {
+                    mp.pause();
+                    playBtn.setBackgroundResource(R.mipmap.ic_stop);
+                }
+                enableSeekBar();
+                break;
+            case R.id.Logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity().getApplicationContext(),SplashActivity.class));
+                break;
         }
-        else {
-            mp.pause();
-            playBtn.setBackgroundResource(R.mipmap.ic_stop);
-        }
-        enableSeekBar();
     }
 
     public void enableSeekBar(){
